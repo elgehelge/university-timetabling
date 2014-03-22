111e26c213629e3db6dd40b80bff702dcf615fa6
elgehelge <helgemunkjacobsen@gmail.com>
2014-03-22 13:02:11 +0100

    Costfunction working - Still we can only add lectures..

--- java_project/src/Solution.java
+++ java_project/src/Solution.java
@@ -6,24 +6,28 @@ import java.util.Random;
 
 public class Solution {
 
-	final static int PENALTY_ROOMCAPACITY			= 1;
-	final static int PENALTY_CURRICULUMCOMPACTNESS	= 2;
-	final static int PENALTY_MINIMUMWORKINGDAYS		= 5;
-	final static int PENALTY_UNSCHEDULED			= 10;
+	final static int COST_ROOMCAPACITY			= 1;
+	final static int COST_ROOMSTABILITY			= 1;
+	final static int COST_CURRICULUMCOMPACTNESS	= 2;
+	final static int COST_MINIMUMWORKINGDAYS	= 5;
+	final static int COST_UNSCHEDULED			= 10;
 
-	private int costTotal;
-	private int costUnscheduled;
-	private int costRoomCapacity;
-	private int costMinimumWorkingDays;
-	private int costCurriculumCompactness;
+	private int countTotalPenalties;
+	private int countUnscheduled;
+	private int countRoomCapacity;
+	private int countMinimumWorkingDays;
+	private int countRoomStability;
+	private int countCurriculumCompactness;
 
 	private Problem problem;
 	private Integer[][][] timetable;			// room + day + period			-> course (The Timetable)
 	private boolean[][][] curriculaTimeslots;	// curriculum + day + period	-> already being thought?
 	private boolean[][][] lecturersTimeslots;	// lecturer + day + period		-> already teaching?
+	private int[] courseUnscheduled;			// course						-> no. of unscheduled lectures
 	private int[][] coursesDays;				// course + day					-> no. of lectures this day
 	private int[] courseDaysBelowMinimum;		// course						-> no. of days below minimum
-	private int[] courseUnscheduled;			// course						-> no. of unscheduled lectures
+	private int[][] coursesRooms;				// course + room				-> no. of lectures in room
+	private int[] courseNoOfRooms;				// course						-> no. of distinct rooms
 	
 	public Solution(Problem problem) {
 		this.problem = problem;
@@ -33,28 +37,32 @@ public class Solution {
 		this.curriculaTimeslots = new boolean[problem.noOfCurricula][problem.noOfDays][problem.periodsPerDay];
 		this.lecturersTimeslots = new boolean[problem.noOfLecturers][problem.noOfDays][problem.periodsPerDay];
 		this.coursesDays = new int[problem.noOfCourses][problem.noOfDays];
+		this.coursesRooms = new int[problem.noOfCourses][problem.noOfRooms];
+		this.courseNoOfRooms = new int[problem.noOfCourses];
 		
 		// Initialize Unscheduled & DaysBelowMinimum
-		int totalUnscheduled = 0;
-		int totalDaysBelowMinimum = 0;
+		int initialUnscheduled = 0;
+		int initialDaysBelowMinimum = 0;
 		this.courseUnscheduled = new int[problem.noOfCourses];
 		this.courseDaysBelowMinimum = new int[problem.noOfCourses];
 		Iterator<Integer> courseIDs = problem.courses.keySet().iterator();
 		while (courseIDs.hasNext()) {
 			int courseID = courseIDs.next();
+			
 			Problem.Course course = problem.courses.get(courseID);
 			this.courseUnscheduled[courseID] = course.noOfLectures;
-			totalUnscheduled += course.noOfLectures;
+			initialUnscheduled += course.noOfLectures;
 			this.courseDaysBelowMinimum[courseID] = course.minWorkDays;
-			totalDaysBelowMinimum += course.minWorkDays;
+			initialDaysBelowMinimum += course.minWorkDays;
 		}
 		
 		// Initialize cost
-		this.costCurriculumCompactness = 0;
-		this.costRoomCapacity = 0;
-		this.costUnscheduled = totalUnscheduled * PENALTY_UNSCHEDULED;
-		this.costMinimumWorkingDays = totalDaysBelowMinimum * PENALTY_MINIMUMWORKINGDAYS;
-		this.costTotal = costUnscheduled + costMinimumWorkingDays;
+		this.countCurriculumCompactness = 0;
+		this.countRoomCapacity = 0;
+		this.countRoomStability = 0;
+		this.countUnscheduled = initialUnscheduled;
+		this.countMinimumWorkingDays = initialDaysBelowMinimum;
+		this.countTotalPenalties = countUnscheduled * COST_UNSCHEDULED + countMinimumWorkingDays * COST_MINIMUMWORKINGDAYS;
 	}
 	
 	public String toString() {
@@ -88,59 +96,63 @@ public class Solution {
 		addLecture(room, day, period, courseID);
 	}
 	
-	private void addLecture(int roomID, int day, int period, int courseID) {
-		if (!isFeasible(roomID, day, period, courseID)) {
+	private void addLecture(int room, int day, int period, int courseID) {
+		if (!isFeasible(room, day, period, courseID)) {
 			//return null; TODO: Make return
 		} else {
-			calcDeltaCost(roomID, day, period, courseID);
-			updateSolution(roomID, day, period, courseID);
+			calcDeltaCost(room, day, period, courseID); // Important that the cost is calculated before the update!
+			updateSolution(room, day, period, courseID);
 		}
 	}
 	
-	private void calcDeltaCost(int roomID, int day, int period, int courseID) {
+	private void calcDeltaCost(int room, int day, int period, int courseID) {
+		// Important that the cost is calculated before the update!
 		Problem.Course course = problem.courses.get(courseID);
 		// RoomCapacity
-		int delta_cost_roomCapacity = Math.max(0, course.noOfStudents - problem.roomCapacity.get(roomID));
-		// CurriculumCompactness (TODO: Maybe this one is to complex to calculate?)
-		int delta_cost_curriculumCompactness = PENALTY_CURRICULUMCOMPACTNESS;
-		Iterator<Integer> curricula = course.curricula.iterator();
-		while (curricula.hasNext()) {
-			int curriculum = curricula.next();
-			for (int adjPeriod : new int[]{period-1, period+1}) {
-				if (adjPeriod > 0 && adjPeriod < problem.periodsPerDay) {
-					if (this.curriculaTimeslots[curriculum][day][adjPeriod]) {
-						delta_cost_curriculumCompactness = 0;
-					}
-				}
-			}
+		int deltaCountRoomCapacity = Math.max(0, course.noOfStudents - problem.roomCapacity.get(room));
+		// CurriculumCompactness (Estimate - This is to complex to calculate!!)
+		int deltaCountCurriculumCompactness = 1;
+		if (isCurriculumCompact(room, day, period, courseID)) {
+			deltaCountCurriculumCompactness = 0;
 		}
 		// MinimumWorkingDays
-		int delta_cost_minWorkDays = 0;
+		int deltaCountMinWorkDays = 0;
 		if (this.coursesDays[courseID][day] == 0) {
 			this.courseDaysBelowMinimum[courseID] += -1;
 			if (this.courseDaysBelowMinimum[courseID] >= 0) { // We improved
-				delta_cost_minWorkDays = -PENALTY_MINIMUMWORKINGDAYS;
+				deltaCountMinWorkDays = -1;
+			}
+		}
+		// RoomStability
+		int deltaCountRoomStability = 0;
+		if (this.coursesRooms[courseID][room] == 0) {
+			this.courseNoOfRooms[courseID] += 1;
+			if (this.courseNoOfRooms[courseID] > 1) { // We used an extra room
+				deltaCountRoomStability = 1;
 			}
 		}
 		// Unscheduled
-		int delta_cost_unscheduled = -PENALTY_UNSCHEDULED;
+		int deltaCountUnscheduled = -1;
 		// Total
-		int delta_cost = delta_cost_curriculumCompactness +
-						 delta_cost_roomCapacity +
-						 delta_cost_minWorkDays +
-						 delta_cost_unscheduled;
+		int deltaTotalCost = deltaCountCurriculumCompactness * COST_CURRICULUMCOMPACTNESS +
+						 deltaCountRoomCapacity * COST_ROOMCAPACITY +
+						 deltaCountRoomStability * COST_ROOMSTABILITY +
+						 deltaCountMinWorkDays * COST_MINIMUMWORKINGDAYS +
+						 deltaCountUnscheduled * COST_UNSCHEDULED;
 		
-		this.costUnscheduled += delta_cost_unscheduled;
-		this.costRoomCapacity += delta_cost_roomCapacity;
-		this.costMinimumWorkingDays += delta_cost_minWorkDays;
-		this.costCurriculumCompactness += delta_cost_curriculumCompactness;
-		this.costTotal += delta_cost;
+		this.countUnscheduled += deltaCountUnscheduled;
+		this.countRoomCapacity += deltaCountRoomCapacity;
+		this.countRoomStability += deltaCountRoomStability;
+		this.countMinimumWorkingDays += deltaCountMinWorkDays;
+		this.countCurriculumCompactness += deltaCountCurriculumCompactness;
+		this.countTotalPenalties += deltaTotalCost;
 	}
 	
-	private void updateSolution(int roomID, int day, int period, int courseID) {
+	private void updateSolution(int room, int day, int period, int courseID) {
+		// Important that the cost is calculated before the update!
 		Problem.Course course = problem.courses.get(courseID);
 		// Register in timetable
-		this.timetable[roomID][day][period] = courseID;
+		this.timetable[room][day][period] = courseID;
 		// Register lecturer
 		this.lecturersTimeslots[course.lecturerID][day][period] = true;
 		// Register curricula
@@ -150,14 +162,16 @@ public class Solution {
 		}
 		// Register an extra lecture that day
 		this.coursesDays[courseID][day] += 1;
+		// Register an extra lecture in that room
+		this.coursesRooms[courseID][room] += 1;
 		// Register that we scheduled a lecture
 		this.courseUnscheduled[courseID] += -1;
 	}
 	
-	private boolean isFeasible(int roomID, int day, int period, int courseID) {
+	private boolean isFeasible(int room, int day, int period, int courseID) {
 		Problem.Course course = problem.courses.get(courseID);
 		// Is there another course scheduled in the same room and same timeslot?
-		if (this.timetable[roomID][day][period] != null) {
+		if (this.timetable[room][day][period] != null) {
 			return false;
 		}
 		// Is all lectures already scheduled?
@@ -188,14 +202,30 @@ public class Solution {
 		// TODO: should return delta-cost & courseID
 	}
 	
+	private boolean isCurriculumCompact(int room, int day, int period, int courseID) {
+		Iterator<Integer> curricula = problem.courses.get(courseID).curricula.iterator();
+		while (curricula.hasNext()) {
+			int curriculum = curricula.next();
+			for (int adjPeriod : new int[]{period-1, period+1}) {
+				if (adjPeriod > 0 && adjPeriod < problem.periodsPerDay) {
+					if (this.curriculaTimeslots[curriculum][day][adjPeriod]) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
 	public String codeJudgeOutput() {
 		String output = "";
-		output += "UNSCHEDULED " + costUnscheduled + "\n";
-		output += "ROOMCAPACITY " + costRoomCapacity + "\n";
-		output += "MINIMUMWORKINGDAYS " + costMinimumWorkingDays + "\n";
-		output += "CURRICULUMCOMPACTNESS " + costCurriculumCompactness + "\n";
+		output += "UNSCHEDULED " + countUnscheduled + "\n";
+		output += "ROOMCAPACITY " + countRoomCapacity + "\n";
+		output += "ROOMSTABILITY " + countRoomStability + "\n";
+		output += "MINIMUMWORKINGDAYS " + countMinimumWorkingDays + "\n";
+		output += "CURRICULUMCOMPACTNESS " + countCurriculumCompactness + "\n";
 		// TODO: RoomStability
-		output += "OBJECTIVE " + costTotal + "\n";
+		output += "OBJECTIVE " + countTotalPenalties + "\n";
 		for (int r = 0; r < problem.noOfRooms; r++) { // loop rooms
 			for (int d = 0; d < problem.noOfDays; d++) { // loop days
 				for (int p = 0; p < problem.periodsPerDay; p++) { // loop periods
